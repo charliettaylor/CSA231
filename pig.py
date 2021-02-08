c523032ebd2e7685c4895f10ce3dfd2967bbd29a
Peekoe <peekoecode@gmail.com>
2021-02-08 15:12:36 -0800

    make parmaters in command line, fix turn passing bugs

--- pig.py
+++ pig.py
@@ -1,11 +1,11 @@
 # The Game of Pig by Charlie Taylor
-
+import sys
 import random as rand
 # side 1, feet 2, back 3, snout 4, ears and nose 5, touching 6
-DICE_BIAS = (0.35, 0.3, 0.2, 0.1, 0.04, 0.01)
+DICE_BIAS = (0.30, 0.25, 0.15, 0.1, 0.1, 0.1)
 
 
-def dice():
+def dice() -> int:
     '''
     rolls weighted 'dice' that represent each part of the pig and returns an
     integer
@@ -20,11 +20,15 @@ def dice():
         result += 1
 
 
-def doubles(rolls: list):
+def doubles(rolls: list) -> int:
     '''
     checks rolled dice to see what kind of double has been thrown, returns the
     point value of the double or 0 if there is no double
+
+    rolls: a list of length 2 that has the player's dice rolls
     '''
+    assert rolls != 0
+    assert len(rolls) == 2
     if rolls[0] == rolls[1]:
         if rolls[0] == 1:
             print("You rolled a Double Sider")
@@ -45,9 +49,10 @@ def doubles(rolls: list):
             print("You rolled a Piggyback")
             return -2
     return 0
+assert doubles([6, 6]) == -2
 
 
-def mixed_combo(score: int, rolls: list):
+def mixed_combo(score: int, rolls: list) -> int:
     '''
     matches roll to score and prints out a message based on that, returns score
     to be added to total player score
@@ -61,11 +66,15 @@ def mixed_combo(score: int, rolls: list):
     print("Mixed Combo:", messages[rolls[0] - 1],
           "and", messages[rolls[1] - 1])
     return (scores[rolls[0] - 1]) + (scores[rolls[1] - 1])
-
+assert mixed_combo(10, [1, 2]) == 5
 
 def game_loop(p1Name: str, p2Name: str, maxScore: int):
     '''
-    continues looping turns until someone wins
+    main game loop that controls turns/score and asks for input from players
+
+    :param p1Name: is retrieved from command line
+    :param p2Name: is retrieved from command line
+    :param maxScore: is retrieved from command line
     '''
     score1 = 0
     score2 = 0
@@ -75,13 +84,18 @@ def game_loop(p1Name: str, p2Name: str, maxScore: int):
     user_input = ''
     # 0 == false
     while p1Play or p2Play:
-        if turn and p1Play:
+        if turn == 1 and p1Play:
+            p1Play = True
             print('-' * 25)
             user_input = input(p1Name + "'s turn, roll or pass? ")
             if user_input.lower() == 'roll':
                 add = take_turn(score1)
                 if add == -2:
                     p1Play = False
+                    p2Play = False
+                elif add <= 0:
+                    score1 += add
+                    turn = 1 - turn
                 else:
                     score1 += add
                     print(p1Name, "score:", score1)
@@ -89,17 +103,18 @@ def game_loop(p1Name: str, p2Name: str, maxScore: int):
             elif user_input.lower() == 'pass':
                 print(p1Name, 'has passed to', p2Name)
                 turn = 1 - turn
-            if score1 >= maxScore:
-                print(p1Name, "has won!")
-                p1Play = False
-                p2Play = False
-        elif p2Play:
+        elif turn == 0 and p2Play:
+            p2Play = True
             print('-' * 25)
             user_input = input(p2Name + "'s turn, roll or pass? ")
             if user_input.lower() == 'roll':
                 add = take_turn(score2)
                 if add == -2:
                     p2Play = False
+                    p1Play = False
+                elif add <= 0:
+                    score2 += add
+                    turn = 1 - turn
                 else:
                     score2 += add
                     print(p1Name, "score:", score1)
@@ -107,18 +122,21 @@ def game_loop(p1Name: str, p2Name: str, maxScore: int):
             elif user_input.lower() == 'pass':
                 print(p2Name, 'has passed to', p1Name)
                 turn = 1 - turn
-            if score1 >= maxScore:
-                print(p2Name, "has won!")
-                p1Play = False
-                p2Play = False
+        if score1 >= maxScore or score2 >= maxScore:
+            
+            print(p1Name if score1 >= maxScore else p2Name, "has won!")
+            p1Play = False
+            p2Play = False
+    print("------------")
     print("Final scores")
-    print(p1Name, "scored", score1)
-    print(p2Name, "scored", score2)
+    print("------------")
+    print(p1Name, "scored:", score1)
+    print(p2Name, "scored:", score2)
 
 
 def take_turn(score: int) -> int:
     '''
-    takes in a player variable of either 1 or 0 and returns a score
+    takes in a player score and returns the amount of points made in a turn
     '''
     turn = [dice(), dice()]
     doubleCheck = doubles(turn)
@@ -129,11 +147,15 @@ def take_turn(score: int) -> int:
 
 
 def main():
-    p1Name = input("Enter player 1's name: ")
-    p2Name = input("Enter player 2's name: ")
-    maxScore = int(input("Enter a score to play to: "))
-    while int(maxScore) <= 0:
-        maxScore = int(input("Please enter a score greater than 0: "))
+    '''
+    retrieves command line arguments and checks score, then runs game loop
+    '''
+    p1Name = sys.argv[1]
+    p2Name = sys.argv[2]
+    try:
+        maxScore = int(sys.argv[3])
+    except ValueError:
+        print("Input for max score must be a number!")
     game_loop(p1Name, p2Name, maxScore)
 
 
